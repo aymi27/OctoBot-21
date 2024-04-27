51f9eb6500c51f982acd19aabd9bd770a2916577
aymi27 <166910421+aymi27@users.noreply.github.com>
2024-04-27 13:52:28 +0800

    Update ai.js

--- commands/ai/ai.js
+++ commands/ai/ai.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = async function ({ dstryr, event, parameters }) {
   try {
   if (parameters.length === 0) {
-            dstryr.sendMessage('ano i sesearch ko lugaw?', event.threadID);
+            dstryr.sendMessage('𝖺𝗇𝗈 𝖻𝖺 𝗂𝗍𝖺𝗍𝖺𝗇𝗈𝗇𝗀 𝗆𝗈 𝗅𝗈𝗏𝖾?', event.threadID);
             return;
         }
         
